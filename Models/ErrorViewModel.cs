2bc5ba0f1d4be86336ec31272f8d575a0badd2da
99prabh <133125732+99prabh@users.noreply.github.com>
2023-07-26 20:49:02 -0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Web1001_TodoApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
